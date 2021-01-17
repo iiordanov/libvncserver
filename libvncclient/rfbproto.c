8a91f51dc6d61673d3816bc004a5e8e706028960
Iordan Iordanov <iiordanov@gmail.com>
2021-01-17 02:16:44 -0500

    Handle rfbVeNCryptPlain authentication subtype.

--- libvncclient/rfbproto.c
+++ libvncclient/rfbproto.c
@@ -1124,6 +1124,7 @@ InitialiseRFBConnection(rfbClient* client)
         if (!HandleVncAuth(client)) return FALSE;
         break;
 
+      case rfbVeNCryptPlain:
       case rfbVeNCryptTLSPlain:
       case rfbVeNCryptX509Plain:
         if (!HandlePlainAuth(client)) return FALSE;
