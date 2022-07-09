23f13a4c768abe47de3c700a11ef1e11034205b4
Iordan Iordanov <iiordanov@gmail.com>
2022-07-09 11:30:35 -0400

    Handle rfbVeNCryptPlain authentication subtype.
        Stability fix for iOS
    
        Added ability to track instance number and whether that instance should maintain connection to the server in the rfbClient struct.
    
        Searching backward for port colon in order to support IPV6 addresses.
    
        Clear error before every SSL_read and SSL_write. Also ensure SSL_MODE_AUTO_RETRY is set. Finally, it seems SSL_ERROR_SYSCALL is retriable, so indicate EAGAIN if it's received.
    
        libvncclient: Implemented callback for certificate approval in tls_openssl.c.
    
        libvncclient: Fix for missing stdio.h include (and hence undeclared snprintf).

--- libvncclient/rfbproto.c
+++ libvncclient/rfbproto.c
@@ -1139,6 +1139,7 @@ InitialiseRFBConnection(rfbClient* client)
         if (!HandleVncAuth(client)) return FALSE;
         break;
 
+      case rfbVeNCryptPlain:
       case rfbVeNCryptTLSPlain:
       case rfbVeNCryptX509Plain:
         if (!HandlePlainAuth(client)) return FALSE;
